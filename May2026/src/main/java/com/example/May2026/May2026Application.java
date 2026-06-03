6d1558df824e2c80259bb5b04e89ab7a3b3a4ded
Balaji <balajim280699@gmail.com>
2026-06-03 09:44:50 +0530

    fix

--- May2026/src/main/java/com/example/May2026/May2026Application.java
+++ May2026/src/main/java/com/example/May2026/May2026Application.java
@@ -1,7 +1,8 @@
-package com.example.May2026.demo;
+package com.example.May2026;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 //import org.springframework.web.bind.annotation.DeleteMapping;
 //import org.springframework.web.bind.annotation.GetMapping;
 //import org.springframework.web.bind.annotation.PostMapping;
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@ComponentScan("com.example.May2026")
 public class May2026Application {
 
 	public static void main(String[] args) {
